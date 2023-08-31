1da67eb94462e9aeef63e60de294ba174e7dc8fc
the-nazmul <98127662+the-nazmul@users.noreply.github.com>
2023-08-31 19:29:59 -0400

    chart

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
+// external links opens in a new tab //
 
+document.addEventListener("DOMContentLoaded", function() {
+  const links = document.querySelectorAll('a');
+
+  links.forEach(link => {
+      // Check if the href starts with "http" or "https://"
+      if (link.getAttribute('href') && link.getAttribute('href').startsWith('http')) {
+          link.setAttribute('target', '_blank'); // Open in a new tab
+          link.setAttribute('rel', 'noopener noreferrer'); // Security measures
+      }
+  });
+});
+
+
+//
 
 
 
